ecf355f0307dc5f9111d774f55a526322bd7bfec
mohd-sahill <mohammedsahil692@gmail.com>
2021-12-08 20:03:38 +0530

    problem 1 FIzzbuzz

---
+++ problem1.js
@@ -0,0 +1,22 @@
+// Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
+
+
+
+function fizzBuzz() {
+  let arr = [];
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("fizzbuzz");
+    } else if (i % 3 === 0) {
+      arr.push("fizz");
+    } else if (i % 5 === 0) {
+      arr.push("buzz");
+    } else {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+}
+
+console.log(fizzBuzz());
