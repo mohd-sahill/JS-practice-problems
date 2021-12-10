a455e66a10b606a5d480de1e422f82058c8b9a48
mohd-sahill <mohammedsahil692@gmail.com>
2021-12-10 22:21:31 +0530

    more

--- file13.js
+++ file13.js
@@ -45,3 +45,51 @@
 //  }
 // replace([1,5,3,5,6,8], 8, 10)
 
+
+// 4Q. 
+// Given two arrays, your function should return single merged array.
+// Example:
+// Input: mergeArray([1,3,5], [2,4,6]) ––> Output: [1,3,5,2,4,6] 
+
+// function mergeArray(arr1,arr2){
+//  let arr3 = [...arr1,...arr2]
+//  console.log(arr3)
+// }
+
+
+// mergeArray([1,3,5,4,5,3], [2,4,6,7,5,6]) 
+
+// 5Q. 
+// Given a string and an index, your function should return the character present at that index in the string.
+// Example:
+// Input: charAt("neoGcamp", 4) ––> Output: c
+
+// function charAt(string,num){
+//    for(i=0;i<=string.length-1;i++){
+//       if(num === i){
+//           console.log(string[i])
+//       }
+//    }
+// } 
+
+// charAt("neoGcamp", 4)
+
+
+
+// 6Q. 
+// Given two dates, your function should return which one comes before the other.
+// Example:
+// Input: minDate('02/05/2021', '24/01/2021') ––> Output: 24/01/2021
+
+// function minDate(date1,date2){
+
+// let d1 = new Date(date1)
+// let d2 = new Date(date2)
+//  if(d1>d2){
+//      console.log(d2.toDateString());
+//  }else{
+//      console.log(d1.toDateString());
+// }
+// }
+// minDate('2021/05/02', '2021/01/24')
+
