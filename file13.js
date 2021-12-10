ee47095ad5a8f13fbcdec5178e30a304b50d0b04
mohd-sahill <mohammedsahil692@gmail.com>
2021-12-10 20:58:09 +0530

    mooooreee problemss!!!!

---
+++ file13.js
@@ -0,0 +1,47 @@
+// Medium mode 
+
+// 1Q.
+// Given an array, your function should return the length of the array.
+// Example:
+// Input: arrayLength([1,5,3,7,8]) ––> Output: 5
+
+// function findLen(array){
+//   return array.length 
+// }
+
+
+// console.log(findLen([1,2,3,4,5]))
+
+
+// 2Q. 
+// Given an array and an item, your function should return the index at which the item is present.
+// Example:
+// Input: indexOf([1,6,3,5,8,9], 3) ––> Output: 2
+
+// function indexOfarr(array,element){
+//  array.filter((e,ind) => {
+//    if(element === e){
+//     console.log(ind)
+//    }
+//  })
+
+// }
+
+// indexOfarr([1,6,3,5,8,9], 6)
+
+
+// 3Q. 
+// Given an array and two numbers, your function should replace all entries of first number in an array with the second number.
+// Example:
+// Input: replace([1,5,3,5,6,8], 5, 10) ––> Output: [1,10,3,10,6,8]
+
+//  function replace(arr,exVal,repVal){
+//    for(i=0 ; i<=arr.length-1 ; i++){
+//        if(arr[i]===exVal){
+//            arr[i] = repVal
+//        }
+//    }
+//    console.log(arr)
+//  }
+// replace([1,5,3,5,6,8], 8, 10)
+
